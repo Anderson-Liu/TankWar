5a6f9afad810ad4d3083b95fd75e060f119fbe47
lhongda <lhongda@live.com>
2014-08-31 11:21:56 +0800

    Make a simple Frame. It's my first try.

--- src/TankClient.java
+++ src/TankClient.java
@@ -1,8 +1,27 @@
+import java.awt.*;
+import java.awt.event.*;
 
-public class TankClient {
+public class TankClient extends Frame{
+	
+	public void launchFrame(){
+		this.setLocation(400, 300);
+		this.setSize(800, 600);
+		this.setVisible(true);
+		this.setResizable(false);
+		this.setTitle("TankWar");
+		this.setBackground(Color.GREEN);
+		this.addWindowListener(new WindowAdapter(){
 
+			@Override
+			public void windowClosing(WindowEvent arg0) {
+				System.exit(0);
+			}
+		});
+	}
+	
+	
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		new TankClient().launchFrame();
 	}
 
 }

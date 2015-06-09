7abacbc5aa8e6325a37d4470be4305ab98edb8b7
Anderson <andersion.v@gmail.com>
2015-06-10 01:19:04 +0800

    为Missile添加图片，导致新bug

--- src/com/Peacesky/Missile.java
+++ src/com/Peacesky/Missile.java
@@ -1,7 +1,9 @@
 package com.Peacesky;
 
 import java.awt.*;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Missile {
     public static final int WIDTH = 10;
@@ -13,6 +15,35 @@ public class Missile {
     Direction dir;
     private TankClient tc;
     private boolean good;
+
+    private static Toolkit tk = Toolkit.getDefaultToolkit();
+    private static Image[] missileImages = null;
+    private static Map<String, Image> imgs = new HashMap<>();
+
+    // 静态代码区
+    // 适合执行变量初始化
+    static {
+        missileImages = new Image[] {
+                tk.getImage(Explode.class.getClassLoader().getResource("image/missileL.gif")),
+                tk.getImage(Explode.class.getClassLoader().getResource("image/missileLU.gif")),
+                tk.getImage(Explode.class.getClassLoader().getResource("image/missileU.gif")),
+                tk.getImage(Explode.class.getClassLoader().getResource("image/missileRU.gif")),
+                tk.getImage(Explode.class.getClassLoader().getResource("image/missileR.gif")),
+                tk.getImage(Explode.class.getClassLoader().getResource("image/missileRD.gif")),
+                tk.getImage(Explode.class.getClassLoader().getResource("image/missileLD.gif"))
+        };
+
+        // static 代码区更灵活 由static代码区才能允许添加以下这段代码
+        // 形成键值对 使用命名代替原来的下标标示  更易于维护
+        imgs.put("L", missileImages[0]);
+        imgs.put("LU", missileImages[1]);
+        imgs.put("U", missileImages[2]);
+        imgs.put("RU", missileImages[3]);
+        imgs.put("R", missileImages[4]);
+        imgs.put("RD", missileImages[5]);
+        imgs.put("D", missileImages[6]);
+        imgs.put("LD", missileImages[7]);
+    }
 
     public Missile(int x, int y, Direction dir) {
         this.x = x;
@@ -27,20 +58,47 @@ public class Missile {
     }
 
     public void draw(Graphics g) {
-        if (!live) return;
-        Color c = g.getColor();
-        if (good)
-            g.setColor(Color.lightGray);
-        else
-            g.setColor(Color.GRAY);
 
-        g.fillOval(x, y, WIDTH, HEIGHT);
-        g.setColor(c);
+        if (!live) {
+            tc.msList.remove(this);
+            return;
+        }
+
+
+        switch (dir) {
+            case L:
+                g.drawImage(imgs.get("L"), x, y, null);
+                break;
+            case LU:
+                g.drawImage(imgs.get("LU"), x, y, null);
+                break;
+            case U:
+                g.drawImage(imgs.get("U"), x, y, null);
+                break;
+            case RU:
+                g.drawImage(imgs.get("RU"), x, y, null);
+                break;
+            case R:
+                g.drawImage(imgs.get("R"), x, y, null);
+                break;
+            case RD:
+                g.drawImage(imgs.get("RD"), x, y, null);
+                break;
+            case D:
+                g.drawImage(imgs.get("D"), x, y, null);
+                break;
+            case LD:
+                g.drawImage(imgs.get("LD"), x, y, null);
+                break;
+            case STOP:
+                break;
+        }
 
         move();
     }
 
-    public void move() {
+    private void move() {
+
         switch (dir) {
             case L:
                 x -= XSPEED;
@@ -70,6 +128,8 @@ public class Missile {
                 x -= XSPEED;
                 y += YSPEED;
                 break;
+            case STOP:
+                break;
         }
 
         if (x < 0 || x > TankClient.Game_wigth || y < 0 || y > TankClient.Game_height) {
@@ -77,6 +137,7 @@ public class Missile {
             tc.msList.remove(this);
         }
     }
+
 
     public boolean isLive() {
         return live;
